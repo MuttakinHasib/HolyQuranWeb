55765cfb2c18b777894f3bab14746a013f93f2b2
MuttakinHasib <hasibmolla28@gmail.com>
2020-08-09 19:31:50 +0600

    solved layout problem

--- src/components/Cards/ChapterCard.js
+++ src/components/Cards/ChapterCard.js
@@ -7,13 +7,15 @@ const ChapterCard = props => {
   return (
     <Link to={`${props?.id}`} className='block card my-3'>
       <div className='d-flex justify-content-between align-items-center'>
-        <div className='number-icon'>
-          <img className='icon' src={icon} alt='' />
-          <p className='number'>{props?.id}</p>
-        </div>
-        <div className='surah-info'>
-          <h5 className='surah-name'>{props?.name}</h5>
-          <span className='translated-name'>{props?.translated_en}</span>
+        <div className='d-flex align-items-center'>
+          <div className='number-icon'>
+            <img className='icon' src={icon} alt='' />
+            <p className='number'>{props?.id}</p>
+          </div>
+          <div className='surah-info ml-3'>
+            <h5 className='surah-name'>{props?.name}</h5>
+            <span className='translated-name'>{props?.translated_en}</span>
+          </div>
         </div>
         <ChapterIcon id={props?.id} />
       </div>
